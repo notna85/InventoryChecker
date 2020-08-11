184154d240073100257a0824b5333888efa2e8fe
DehliFin <42273903+DehliFin@users.noreply.github.com>
2020-08-11 10:58:34 +0200

    inserts all products

---
+++ SQLscripts/Freezer product Inserts.sql
@@ -0,0 +1,240 @@
+insert into product (PName, Category) values ('Tørkager','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Tørkager','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Tørkager','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Tørkager','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Tørkager','Æsker',0);
+
+insert into product (PName, Category) values ('Rørkager','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Rørkager','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rørkager','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rørkager','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rørkager','Æsker',0);
+
+insert into product (PName, Category) values ('Romkugler','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Romkugler','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Romkugler','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Romkugler','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Romkugler','Æsker',0);
+
+insert into product (PName, Category) values ('Bryllupkager','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Bryllupkager','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bryllupkager','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bryllupkager','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bryllupkager','Æsker',0);
+
+insert into product (PName, Category) values ('Fromage','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Fromage','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Fromage','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Fromage','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Fromage','Æsker',0);
+
+insert into product (PName, Category) values ('Roulader','Kager');
+insert into ProductAmount (product, StorageType, Amount)values ('Roulader','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Roulader','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Roulader','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Roulader','Æsker',0);
+
+insert into product (PName, Category) values ('Jordbær','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Jordbær','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Jordbær','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Jordbær','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Jordbær','Æsker',0);
+
+insert into product (PName, Category) values ('Hindbær','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Hindbær','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hindbær','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hindbær','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hindbær','Æsker',0);
+
+insert into product (PName, Category) values ('Solbær','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Solbær','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Solbær','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Solbær','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Solbær','Æsker',0);
+
+insert into product (PName, Category) values ('Stikkelsbær','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Stikkelsbær','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stikkelsbær','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stikkelsbær','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stikkelsbær','Æsker',0);
+
+insert into product (PName, Category) values ('Brombær','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Brombær','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Brombær','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Brombær','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Brombær','Æsker',0);
+
+insert into product (PName, Category) values ('Havtorn','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Havtorn','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Havtorn','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Havtorn','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Havtorn','Æsker',0);
+
+insert into product (PName, Category) values ('Rabarber','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Rabarber','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rabarber','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rabarber','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rabarber','Æsker',0);
+
+insert into product (PName, Category) values ('Ribs','Frugt');
+insert into ProductAmount (product, StorageType, Amount)values ('Ribs','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ribs','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ribs','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ribs','Æsker',0);
+
+insert into product (PName, Category) values ('Franskbrød','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Franskbrød','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Franskbrød','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Franskbrød','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Franskbrød','Æsker',0);
+
+insert into product (PName, Category) values ('Præbagt','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Præbagt','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Præbagt','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Præbagt','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Præbagt','Æsker',0);
+
+insert into product (PName, Category) values ('Bagt','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Bagt','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bagt','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bagt','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bagt','Æsker',0);
+
+insert into product (PName, Category) values ('Kold hævede','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Kold hævede','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Kold hævede','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Kold hævede','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Kold hævede','Æsker',0);
+
+insert into product (PName, Category) values ('Special brød','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Special brød','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Special brød','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Special brød','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Special brød','Æsker',0);
+
+insert into product (PName, Category) values ('Rugbrød','Brød/Dej');
+insert into ProductAmount (product, StorageType, Amount)values ('Rugbrød','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rugbrød','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rugbrød','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rugbrød','Æsker',0);
+
+insert into product (PName, Category) values ('Dej','Wienerbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Dej','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Dej','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Dej','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Dej','Æsker',0);
+
+insert into product (PName, Category) values ('Chokoladeboller','Wienerbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Chokoladeboller','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Chokoladeboller','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Chokoladeboller','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Chokoladeboller','Æsker',0);
+
+insert into product (PName, Category) values ('Lagkagebunde','Halvfabrikata');
+insert into ProductAmount (product, StorageType, Amount)values ('Lagkagebunde','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Lagkagebunde','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Lagkagebunde','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Lagkagebunde','Æsker',0);
+
+insert into product (PName, Category) values ('Bunde','Halvfabrikata');
+insert into ProductAmount (product, StorageType, Amount)values ('Bunde','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bunde','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bunde','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Bunde','Æsker',0);
+
+insert into product (PName, Category) values ('Mørdej','Halvfabrikata');
+insert into ProductAmount (product, StorageType, Amount)values ('Mørdej','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Mørdej','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Mørdej','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Mørdej','Æsker',0);
+
+insert into product (PName, Category) values ('Butterdej','Halvfabrikata');
+insert into ProductAmount (product, StorageType, Amount)values ('Butterdej','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Butterdej','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Butterdej','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Butterdej','Æsker',0);
+
+insert into product (PName, Category) values ('Ramme kager','Halvfabrikata');
+insert into ProductAmount (product, StorageType, Amount)values ('Ramme kager','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ramme kager','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ramme kager','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Ramme kager','Æsker',0);
+
+insert into product (PName, Category) values ('Sandwichboller','Morgenbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Sandwichboller','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Sandwichboller','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Sandwichboller','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Sandwichboller','Æsker',0);
+
+
+insert into product (PName, Category) values ('Rundstykker','Morgenbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Rundstykker','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rundstykker','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rundstykker','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Rundstykker','Æsker',0);
+
+
+insert into product (PName, Category) values ('Horn/Gifler','Morgenbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Horn/Gifler','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn/Gifler','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn/Gifler','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn/Gifler','Æsker',0);
+
+
+insert into product (PName, Category) values ('Grovboller','Morgenbrød');
+insert into ProductAmount (product, StorageType, Amount)values ('Grovboller','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Grovboller','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Grovboller','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Grovboller','Æsker',0);
+
+insert into product (PName, Category) values ('Top','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Top','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Top','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Top','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Top','Æsker',0);
+
+insert into product (PName, Category) values ('Horn','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Horn','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Horn','Æsker',0);
+
+insert into product (PName, Category) values ('Vugge','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Vugge','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Vugge','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Vugge','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Vugge','Æsker',0);
+
+insert into product (PName, Category) values ('Stykker','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Stykker','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stykker','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stykker','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stykker','Æsker',0);
+
+insert into product (PName, Category) values ('Stænger','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Stænger','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stænger','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stænger','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Stænger','Æsker',0);
+
+insert into product (PName, Category) values ('Konfekt','Kransekage');
+insert into ProductAmount (product, StorageType, Amount)values ('Konfekt','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Konfekt','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Konfekt','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Konfekt','Æsker',0);
+
+insert into product (PName, Category) values ('Hvedemel','Mel');
+insert into ProductAmount (product, StorageType, Amount)values ('Hvedemel','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hvedemel','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hvedemel','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Hvedemel','Æsker',0);
+
+insert into product (PName, Category) values ('Italiansk','Mel');
+insert into ProductAmount (product, StorageType, Amount)values ('Italiansk','Kasser',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Italiansk','Pladder',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Italiansk','stk',0);
+insert into ProductAmount (product, StorageType, Amount)values ('Italiansk','Æsker',0);
+
+
+
+
